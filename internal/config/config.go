19241454f5fa2c60443775028f3c5123fed67738
AbdelrahmanDwedar <dedou2005@gmail.com>
2026-05-03 00:37:05 +0300

    feat(config): implement dynamic configuration loader

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type DatabaseConfig struct {
+	Driver   string `yaml:"driver"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"`
+}
+
+type MigrationsConfig struct {
+	Parser string `yaml:"parser"`
+	Dir    string `yaml:"dir"`
+}
+
+type Config struct {
+	Database   DatabaseConfig   `yaml:"database"`
+	Migrations MigrationsConfig `yaml:"migrations"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
